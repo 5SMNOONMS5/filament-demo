a33cfb2a4e310ab0969640b1acb877bb55732df4
tasb00429 <tasb00429@gmail.com>
2024-09-26 15:24:24 +0800

    Update

---
+++ deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo "這腳本是每次在 git 更新的時後會去清理 cache, 然後在 cache 新的 components, icons, route, view, config, api doc, 最後跑測試。"
+
+# Install composer dependencies
+#composer install
+
+# Build assets
+npm run build
+
+# clear cache
+php artisan optimize:clear
+
+# clear filament cached components
+php artisan filament:clear-cached-components
+
+# clear filament permission cache
+php artisan cache:forget spatie.permission.cache
+
+# generate filament icons cache
+php artisan icons:cache
+
+# generate filament components cache
+php artisan filament:cache-components
+
+# generate route cache
+php artisan route:cache
+
+# generate view cache
+# generate config cache
+php artisan optimize
+
+# generate api doc
+php artisan custom:scribe:generate
+
+# run test
+#php artisan test
+
